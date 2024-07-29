f46087a4a2c1587324532e2a6629f174d8c63bd3
dsa302010 <164147796+dsa302010@users.noreply.github.com>
2024-07-29 08:04:41 +0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -23,8 +23,8 @@ from openpilot.dp_ext.selfdrive.controls.lib.curve_speed_limiter import CurveSpe
 from openpilot.dp_ext.selfdrive.controls.lib.personalized_accel_controller import PersonalizedAccelController
 
 LON_MPC_STEP = 0.2  # first step is 0.2s
-A_CRUISE_MIN = -0.55
-A_CRUISE_MAX_VALS = [1.2, 0.8, 0.4, 0.2]
+A_CRUISE_MIN = -0.9
+A_CRUISE_MAX_VALS = [1.15, 0.85, 0.4, 0.2]
 A_CRUISE_MAX_BP = [0., 10.0, 25., 40.]
 CONTROL_N_T_IDX = ModelConstants.T_IDXS[:CONTROL_N]
 
