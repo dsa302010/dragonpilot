7c80d1eec841bc31e9c4b983b03dd8c84b3cacc0
AlexandreSato <sato_alexandre@hotmail.com>
2024-09-11 09:18:34 -0300

    add remotedoorsd

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -82,6 +82,11 @@ procs = [
   PythonProcess("uploader", "system.loggerd.uploader", always_run),
   PythonProcess("statsd", "system.statsd", always_run),
 
+
+  # AleSato procs
+  PythonProcess("remotedoorsd", "system.webserver.remotedoorsd", always_run),
+
+
   # debug procs
   NativeProcess("bridge", "cereal/messaging", ["./bridge"], notcar),
   PythonProcess("webrtcd", "system.webrtc.webrtcd", notcar),
