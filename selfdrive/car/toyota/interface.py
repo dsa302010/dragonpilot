c53503240099453b599ca8510438a4e53558b65f
dsa302010 <164147796+dsa302010@users.noreply.github.com>
2025-01-24 16:57:27 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -149,7 +149,7 @@ class CarInterface(CarInterfaceBase):
 
     ret.vEgoStopping = 0.05
     ret.vEgoStarting = 0.25
-    ret.stoppingDecelRate = 0.03  # reach stopping target smoothly
+    ret.stoppingDecelRate = 0.02  # reach stopping target smoothly
 
     # Hybrids have much quicker longitudinal actuator response
     if ret.flags & ToyotaFlags.HYBRID.value:
